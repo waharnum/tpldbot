1a1ffa354358d62e8bc0d9a602407913d35978d0
Alan Harnum <waharnum@gmail.com>
2015-11-15 10:13:30 -0500

    A fully 'working' implementation

--- tpldbot.py
+++ tpldbot.py
@@ -2,6 +2,7 @@ from lxml import etree
 from random import randint
 import sys
 import tweepy
+import time
 
 # Expected usage: $ "python tpldbot.py [consumer_key] [consumer_secret] [access_token] [access_token_secret]"
 
@@ -15,56 +16,67 @@ access_token_secret = str(sys.argv[4])
 
 result_feed_URL = "http://www.torontopubliclibrary.ca/rss.jsp?N=38550&Erp=0"
 
-# Get the feed without records to make finding the total records faster
-tree = etree.parse(result_feed_URL)
+# Run in an infinite loop, make a tweet every hour
 
-# Get the total results
-total_results = tree.find('channel').find('results').find('total-results').text
+while True:
 
-# print(total_results)
+    # Get the feed without records to make finding the total records faster
+    tree = etree.parse(result_feed_URL)
 
-# Generate a random number from the total results
+    # Get the total results
+    total_results = tree.find('channel').find('results').find('total-results').text
 
-image_number = randint(1, int(total_results))
+    # print(total_results)
 
-print ("You should get image #" + str(image_number))
+    # Generate a random number from the total results
 
-# Get the record associated with that random number
+    image_number = randint(1, int(total_results))
+    #
+    # print ("You should get image #" + str(image_number))
 
-record_feed_URL = "http://www.torontopubliclibrary.ca/rss.jsp?N=38550&Erp=1&No=" + str(image_number)
+    # Get the record associated with that random number
 
-record_tree = etree.parse(record_feed_URL)
+    record_feed_URL = "http://www.torontopubliclibrary.ca/rss.jsp?N=38550&Erp=1&No=" + str(image_number)
 
-item = record_tree.find('channel').find('item')
+    record_tree = etree.parse(record_feed_URL)
 
-item_title = item.find('title').text
+    item = record_tree.find('channel').find('item')
 
-item_link = item.find('link').text
+    item_title = item.find('title').text
 
-item_record = item.find('record')
+    item_link = item.find('link').text
 
-item_id = item_record.find('recordId').text
+    item_record = item.find('record')
 
-item_image_file_name = item_record.xpath("./attributes/attr[@name='p_file_name']/text()")
+    item_id = item_record.find('recordId').text
 
-# http://static.torontopubliclibrary.ca/da/images/MC/pcr-473.jpg
+    item_image_file_name = item_record.xpath("./attributes/attr[@name='p_file_name']/text()")
 
-# We have to convert the file name to lowercase
+    # http://static.torontopubliclibrary.ca/da/images/MC/pcr-473.jpg
 
-item_image_URL = "http://static.torontopubliclibrary.ca/da/images/MC/" + item_image_file_name[0].lower()
+    # We have to convert the file name to lowercase
 
-print("Title: " + item_title)
-print("Record ID: " + item_id)
-print("Link: " + item_link)
-print("Image URL:" + item_image_URL)
+    item_image_URL = "http://static.torontopubliclibrary.ca/da/images/MC/" + item_image_file_name[0].lower()
+    #
+    # print("Title: " + item_title)
+    # print("Record ID: " + item_id)
+    # print("Link: " + item_link)
+    # print("Image URL:" + item_image_URL)
 
-# Make a tweet from it
+    # Make a tweet from it
 
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+    tweet_URL_max_length = 40
+    tweet_title_trim_length = 140 - tweet_URL_max_length
 
-api = tweepy.API(auth)
+    tweet = item_title[:tweet_title_trim_length] + " " + item_link
+    
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-tweet = item_title + " " + item_link
-print(tweet)
-api.update_status(status=tweet)
+    api = tweepy.API(auth)
+
+
+    print("I'm making a tweet!")
+    print(tweet)
+    api.update_status(status=tweet)
+    time.sleep(3600)
